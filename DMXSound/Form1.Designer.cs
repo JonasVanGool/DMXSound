1f3bd9f8dd3309640ce514d704a04ee4e997b683
JonasVanGool <vangooljonas@gmail.com>
2015-11-01 22:17:03 +0100

    Dmxsound startup

---
+++ DMXSound/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace DMXSound
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.AllowDrop = true;
+            this.chart1.BackColor = System.Drawing.Color.Transparent;
+            this.chart1.BorderlineColor = System.Drawing.Color.Transparent;
+            this.chart1.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            chartArea1.AxisX.Maximum = 1023D;
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.AxisY.Maximum = 32769D;
+            chartArea1.AxisY.Minimum = -32769D;
+            chartArea1.Name = "ChartArea1";
+            chartArea1.Position.Auto = false;
+            chartArea1.Position.Height = 94F;
+            chartArea1.Position.Width = 78.48222F;
+            chartArea1.Position.X = 3F;
+            chartArea1.Position.Y = 3F;
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(-11, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(675, 209);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "cv";
+            // 
+            // chart2
+            // 
+            this.chart2.AllowDrop = true;
+            this.chart2.BackColor = System.Drawing.Color.Transparent;
+            this.chart2.BorderlineColor = System.Drawing.Color.Transparent;
+            this.chart2.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            chartArea2.AxisX.Maximum = 1023D;
+            chartArea2.AxisX.Minimum = 0D;
+            chartArea2.AxisY.Maximum = 32769D;
+            chartArea2.AxisY.Minimum = -32769D;
+            chartArea2.Name = "ChartArea1";
+            chartArea2.Position.Auto = false;
+            chartArea2.Position.Height = 94F;
+            chartArea2.Position.Width = 78.48222F;
+            chartArea2.Position.X = 3F;
+            chartArea2.Position.Y = 3F;
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(-11, 212);
+            this.chart2.Name = "chart2";
+            this.chart2.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(675, 209);
+            this.chart2.TabIndex = 1;
+            this.chart2.Text = "cchart1.Series[\"Series1\"].Points.Remove(chart1.Series[\"Series1\"].Points.First());" +
+    "";
+            // 
+            // chart3
+            // 
+            this.chart3.AllowDrop = true;
+            this.chart3.BackColor = System.Drawing.Color.Transparent;
+            this.chart3.BorderlineColor = System.Drawing.Color.Transparent;
+            this.chart3.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            chartArea3.AxisX.Maximum = 512D;
+            chartArea3.AxisX.Minimum = 0D;
+            chartArea3.AxisX.ScaleView.SizeType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea3.AxisY.IsLogarithmic = true;
+            chartArea3.AxisY.Maximum = 1000D;
+            chartArea3.AxisY.Minimum = 1D;
+            chartArea3.AxisY.MinorGrid.Interval = double.NaN;
+            chartArea3.AxisY.MinorGrid.IntervalOffsetType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea3.AxisY.MinorTickMark.Enabled = true;
+            chartArea3.Name = "ChartArea1";
+            chartArea3.Position.Auto = false;
+            chartArea3.Position.Height = 94F;
+            chartArea3.Position.Width = 78.48222F;
+            chartArea3.Position.X = 3F;
+            chartArea3.Position.Y = 3F;
+            this.chart3.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart3.Legends.Add(legend3);
+            this.chart3.Location = new System.Drawing.Point(-11, 409);
+            this.chart3.Name = "chart3";
+            this.chart3.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart3.Series.Add(series3);
+            this.chart3.Size = new System.Drawing.Size(675, 209);
+            this.chart3.TabIndex = 2;
+            this.chart3.Text = "cchart1.Series[\"Series1\"].Points.Remove(chart1.Series[\"Series1\"].Points.First());" +
+    "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 617);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+
+    }
+}
+
---
+++ DMXSound/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+
+namespace DMXSound
+{
+    public partial class Form1 : Form
+    {
+        private static int fftLength = 1024; // NAudio fft wants powers of two!
+        private long counter = 0;
+
+        // There might be a sample aggregator in NAudio somewhere but I made a variation for my needs
+        private SampleAggregator sampleAggregator = new SampleAggregator(fftLength);
+        NAudio.Wave.WaveIn sourceStream = null;
+        NAudio.Wave.DirectSoundOut waveOut = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            sampleAggregator.FftCalculated += new EventHandler<FftEventArgs>(FftCalculated);
+            sampleAggregator.PerformFFT = true;
+
+            sourceStream = new NAudio.Wave.WaveIn();
+            sourceStream.DeviceNumber = 0;
+            sourceStream.WaveFormat = new NAudio.Wave.WaveFormat(44100, NAudio.Wave.WaveIn.GetCapabilities(0).Channels);
+            sourceStream.DataAvailable += OnDataAvailable;
+
+            //NAudio.Wave.WaveInProvider waveIn = new NAudio.Wave.WaveInProvider(sourceStream);
+
+            //waveOut = new NAudio.Wave.DirectSoundOut();
+           // waveOut.Init(waveIn);
+
+            sourceStream.StartRecording();
+           // waveOut.Play();
+
+        }
+
+        void OnDataAvailable(object sender, WaveInEventArgs e)
+        {
+            int numberOfChannels = 2;
+            int numberOfbytes = 2;
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(new EventHandler<WaveInEventArgs>(OnDataAvailable), sender, e);
+            }
+            else
+            {
+                byte[] buffer = e.Buffer;
+                int bytesRecorded = e.BytesRecorded;
+                int bufferIncrement = sourceStream.WaveFormat.BlockAlign;
+
+                for (int i = 0; i < e.Buffer.Length/4; i = i + 4)
+                {
+
+                    float sampleL = Convert.ToSingle(buffer[i] + (buffer[i + 1]<<8));
+                    float sampleR = Convert.ToSingle(buffer[i+2] + (buffer[i + 3]<<8));
+
+                    if (sampleL > 32767)
+                        sampleL = sampleL - 65536;
+                    if (sampleR > 32767)
+                        sampleR = sampleR - 65536;
+
+                    sampleAggregator.Add(sampleL);
+                    chart1.Series["Series1"].Points.AddY(sampleL);
+                    chart2.Series["Series1"].Points.AddY(sampleR);
+                    if (counter > 1023)
+                    {
+                        chart1.Series["Series1"].Points.Remove(chart1.Series["Series1"].Points.First());
+                        chart2.Series["Series1"].Points.Remove(chart2.Series["Series1"].Points.First());
+                    }
+                    else
+                    {
+                        counter++;
+                    }
+                }
+            }
+        }
+
+        void FftCalculated(object sender, FftEventArgs e)
+        {
+            chart3.Series["Series1"].Points.Clear();
+            for (var i = 0; i < e.Result.Length/2; i++)
+            {
+               chart3.Series["Series1"].Points.AddY(Math.Sqrt(e.Result[i].X * e.Result[i].X + e.Result[i].Y * e.Result[i].Y));
+            }
+        }
+    }
+}
